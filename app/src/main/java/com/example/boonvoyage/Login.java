ee9b2ffb3f05a34f51eacce02498233e2efce747
Ujjwal <y20cs161@rvrjc.ac.in>
2023-11-04 01:24:16 +0530

    Listeners added

--- app/src/main/java/com/example/boonvoyage/Login.java
+++ app/src/main/java/com/example/boonvoyage/Login.java
@@ -3,12 +3,54 @@ package com.example.boonvoyage;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.google.android.material.textfield.TextInputEditText;
 
 public class Login extends AppCompatActivity {
-
+    TextInputEditText emailId,password;
+    Button login,register,forgetPassword;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+//        theme
+
+//        linking of UI
+
+        emailId = findViewById(R.id.emailfield);
+        password = findViewById(R.id.passwordfield);
+
+        login = findViewById(R.id.sign_in);
+        register = findViewById(R.id.sign_up);
+        forgetPassword = findViewById(R.id.pswdfrgt);
+
+//        Listeners of Buttons
+
+        login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+//                Login Button Action
+            }
+        });
+
+        register.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+//                register action
+            }
+        });
+
+        forgetPassword.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+//        forgetPassword
+            }
+        });
+
+
+
     }
 }
